40bfe28e7a01f4b27144a23b5e888f5d6aeacbb2
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2020-07-09 13:31:17 -0300

    Inclusao da classe Layout

--- b3.py
+++ b3.py
@@ -1,36 +1,80 @@
 import tensorflow as tf
 from tensorflow import keras
 
-#arq = open('COTAHIST_A2020.txt','r')
+class Layout:
+    "Classe contendo o posicionamento de cada dado presente na serie historica da B3"
+    posTIPREG = [0, 2] # Tipo de registro
+    posDataPregao = [2, 10] # Data do pregao
+    posCODBDI = [10, 12] # Codigo BDI
+    posCODNEG = [12, 24] # Codigo de negociacao do papel
+    posTPMERC = [24, 27] # Tipo de mercado
+    posNOMRES = [27, 39] # Nome resumido da empresa emissora do papel
+    posESPECI = [39, 49] # Especificado do papel
+    posPRAZOT = [49, 52] # Prazo em dias do mercado a termo
+    posMODREF = [52, 56] # Moeda de referencia
+    posPREABE = [56, 69] # Preco de abertura do papel-mercado no pregao
+    posPREMAX = [69, 82] # Preco maximo do papel-mercado no pregao
+    posPREMIN = [82, 95] # Preco minimo do papel-mercado no pregao
+    posPREMED = [95, 108] # Preco medio do papel-mercado no pregao
+    posPREULT = [108, 121] # Preco do ultimo negocio do papel-mercado no pregao
+    posPREOFC = [121, 134] # Preco da melhor oferta de compra do papel-mercado
+    posPREOFV = [134, 147] # Preco da melhor oferta de venda do papel-mercado
+    posTOTNEG = [147, 152] # Numero de negocios efetuados com o papel-mercado no pregao
+    posQUATOT = [152, 170] # Quantidade de titulos negociados neste papel-mercado
+    posVOLTOT = [170, 188] # Volume de titulos negociados neste papel-mercado
+    posPREEXE = [188, 201] # Preco de exercicio para o mercado de opcoes ou valor do contrato para o mercado de termo secundario
+    posINDOPC = [201, 202] # Indicador de correcao de precos de exercicios ou valores de contrato para os mercados de opcoes ou termo secundario
+    posDATVEN = [202, 210] # Data do vencimento para os mercados de opcoes ou termo secundario
+    posFATCOT = [210, 217] # Fator de cotacao do papel
+    posPTOEXE = [217, 230] # Preco de exercicio em pontos para opcoes referenciadas em dolar ou valor de contrato em pontos para termo secundario
+    posCODISI = [230, 242] # Codigo do papel no sistema ISIN ou codigo interno do papel
+    posDISMES = [242, 245] # Numero de distribuicao do papel
 
 # header 00
 # trailer 99
 # valor dos ativos 01
 
-#posDataPregao = [2, 10]
 datasPregao = []
-#posNomeResumido = [27, 39]
-nomesResumidos = []
-#posPrecoAbertura = [56, 69]
-precosAbertura = []
-#posPrecoFechamento = [108, 121]
-precosFechamento = []
+CODBDI = []
+CODNEG = []
+TPMERC = []
+NOMRES = []
+ESPECI = []
+PRAZOT = []
+MODREF = []
+PREABE = []
+PREMAX = []
+PREMIN = []
+PREMED = []
+PREULT = []
+PREOFC = []
+PREOFV = []
+TOTNEG = []
+QUATOT = []
+VOLTOT = []
+PREEXE = []
+INDOPC = []
+DATVEN = []
+FATCOT = []
+PTOEXE = []
+CODISI = []
+DISMES = []
 
 # arquivo txt contendo a série histórica disponível em:
 # http://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/historico/mercado-a-vista/series-historicas/
 with open("../COTAHIST_A2020.txt") as file: 
     for line in file:
         if line[0:2] != '00' and line[0:2] != '99':
-            datasPregao.append(line[2:10])
-            nomesResumidos.append(line[27:39])
-            precosAbertura.append(line[56:69])
-            precosFechamento.append(line[108:121])
+            datasPregao.append(line[Layout.posDataPregao[0]:Layout.posDataPregao[1]])
+            NOMRES.append(line[Layout.posNOMRES[0]:Layout.posNOMRES[1]])
+            PREABE.append(line[Layout.posPREABE[0]:Layout.posPREABE[1]])
+            PREULT.append(line[Layout.posPREULT[0]:Layout.posPREULT[1]])
 
 ativos = []
 ativos.append(datasPregao)
-ativos.append(nomesResumidos)
-ativos.append(precosAbertura)
-ativos.append(precosFechamento)
+ativos.append(NOMRES)
+ativos.append(PREABE)
+ativos.append(PREULT)
 
 print('Data do pregao: ', ativos[0][1])
 print('Nome resumido da empresa: ', ativos[1][1])
